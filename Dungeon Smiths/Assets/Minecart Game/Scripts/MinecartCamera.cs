ac3afbdb9ff4ab2008e2cc6e5ca5080889f2108a
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2020-02-18 01:12:18 -0800

    basic stuff for minecart game

---
+++ Dungeon Smiths/Assets/Minecart Game/Scripts/MinecartCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinecartCamera : MonoBehaviour
+{
+    public GameObject minecart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Locking camera on the minecart
+        transform.position = minecart.transform.position;
+        Vector3 pos = transform.position;
+        pos.z = -30;
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = minecart.transform.position;
+        Vector3 pos = transform.position;
+        pos.z = -30;
+        transform.position = pos;
+    }
+}
