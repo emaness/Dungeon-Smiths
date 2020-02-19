7a1562b6190388705910a6033f079c7646a8e36a
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2020-02-18 19:33:58 -0800

    bit of progress (still need finalize the gravity, jumping, and end state

--- Dungeon Smiths/Assets/Minecart Game/Scripts/MinecartMove.cs
+++ Dungeon Smiths/Assets/Minecart Game/Scripts/MinecartMove.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class MinecartMove : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D m_Rigidbody;
+
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += 2;
-        transform.position = pos;
+        m_Rigidbody.velocity = new Vector2(100.0f, 0.0f);
+        //Vector2 vec = new Vector2(1.0f, 0.0f);
+        //m_Rigidbody.(vec * 100, ForceMode2D.Force);
+        //Vector3 pos = transform.position;
+        //pos.x += 2;
+        //transform.position = pos;
     }
 }
