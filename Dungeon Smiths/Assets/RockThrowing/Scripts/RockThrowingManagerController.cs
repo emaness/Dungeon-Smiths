3aa7441a2271a72303e0044d3d99ac9dc3ede5d0
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2020-02-29 15:52:01 -0800

    merge

---
+++ Dungeon Smiths/Assets/RockThrowing/Scripts/RockThrowingManagerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+public class AngryRocksManagerController : MonoBehaviour
+{
+    public void Quit()
+    {
+        SceneManager.UnloadSceneAsync("RockThrowing");
+
+        Scene mt = SceneManager.GetSceneByName("Level1");
+        foreach (GameObject obj in mt.GetRootGameObjects())
+        {
+            obj.SetActive(true);
+        }
+    }
+}
