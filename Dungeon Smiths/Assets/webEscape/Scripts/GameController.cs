3fc15fe3641f08250fa6b44f382d0bd96d303265
Jamila <abuomar@usc.edu>
2020-03-01 15:21:18 -0800

    updated my minigames to end and go back to level 1

--- Dungeon Smiths/Assets/webEscape/Scripts/GameController.cs
+++ Dungeon Smiths/Assets/webEscape/Scripts/GameController.cs
@@ -12,24 +12,51 @@ public class GameController : MonoBehaviour
 	[SerializeField] public ballDrop[] balls;
 	public int ballCount = 6;
 	[SerializeField] public Text countText;
-
+	[SerializeField] public Text YouWin;
+	[SerializeField] public Text TryAgain;
 	void Start()
 	{
 		ballCount = 6;
+		YouWin.gameObject.SetActive(false);
+		TryAgain.gameObject.SetActive(false);
 		countText.text = "Balls Left: " + ballCount.ToString();
 	}
 
+	private IEnumerator DoWin()
+	{
+		yield return new WaitForSeconds(2.5f);
+
+		SceneManager.UnloadSceneAsync("SpiderScene");
+
+		Scene mt = SceneManager.GetSceneByName("Level1");
+		foreach (GameObject obj in mt.GetRootGameObjects())
+		{
+			if (obj.name != "PauseMenu")
+			{
+				obj.SetActive(true);
+			}
+		}
+	}
+
+	private IEnumerator DoRestart()
+	{
+		yield return new WaitForSeconds(2.5f);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
 	void Update()
 	{
 		if(spiderCount == 0)
 		{
 			//game won
-			print("win");
+			YouWin.gameObject.SetActive(true);
+			StartCoroutine("DoWin");
 		}
 		if(spiderCount > 0 && ballCount < 1 && balls[0].transform.position.y < -8 )
 		{
 			//restart
-			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			TryAgain.gameObject.SetActive(true);
+			StartCoroutine("DoRestart");
+
 		}
 
 		if (Input.GetMouseButtonDown(0) && ballCount > 0)
