93144cd68d80f79e86a8107ce95f8ee1ba74757a
sashavol <sashavol@sashavol.com>
2020-03-04 00:40:29 -0800

    finished rock throwing game

--- Dungeon Smiths/Assets/RockThrowing/Scripts/RockController.cs
+++ Dungeon Smiths/Assets/RockThrowing/Scripts/RockController.cs
@@ -5,28 +5,39 @@ using UnityEngine.UI;
 
 public class RockController : MonoBehaviour
 {
+    public GameObject rockExplosionPrefab;
     public Image skelBar;
+
+    private void Die()
+    {
+        Instantiate(rockExplosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            Destroy(gameObject);
-        } else if(collision.gameObject.CompareTag("Skeleton"))
+            Die();
+        } else if(collision.gameObject.CompareTag("SkeletonWeakPoint"))
         {
-            var skel = collision.gameObject;
+            var skel = GameObject.FindWithTag("Skeleton");
             var ctrl = skel.GetComponent<SkeletonController>();
             ctrl.Health -= 1;
-            
+
             skelBar.rectTransform.localScale = new Vector3(ctrl.Health / (float)ctrl.InitialHealth, 1.0f, 1.0f);
 
             if (ctrl.Health == 0)
             {
                 transform.parent.gameObject.SendMessage("StartWin");
-                Destroy(skel);
+                ctrl.Die();
             }
            
-            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            Die();
+        } else if(collision.gameObject.CompareTag("Skeleton"))
+        {
+            Die();
         }
     }
 }
---
+++ Dungeon Smiths/Assets/RockThrowing/Scripts/SkeletonWeakPointController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonWeakPointController : MonoBehaviour
+{
+    private float t;
+
+    private void Start()
+    {
+        t = 0.0f;
+    }
+
+    void Update()
+    {
+        // fade effect
+        float opacity = 0.5f + 0.5f * ((Mathf.Cos(t * 10.0f) + 1.0f) / 2.0f);
+        var sr = GetComponent<SpriteRenderer>();
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, opacity);
+        t += Time.deltaTime;
+
+        // face the camera
+        transform.LookAt(transform.position - new Vector3(0, 0, 1));
+    }
+}
