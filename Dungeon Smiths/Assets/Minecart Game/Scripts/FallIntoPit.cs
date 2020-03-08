d7d3b88cc1a3a836e024157477ffdf48c5459796
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2020-03-08 03:33:48 -0700

    jump done, dying to pits done, need to fix movement and add win condition, if time permits do ui/randomizing of terrain

---
+++ Dungeon Smiths/Assets/Minecart Game/Scripts/FallIntoPit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallIntoPit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.UnloadSceneAsync("Minecart Game");
+        SceneManager.LoadScene("Minecart Game");
+    }
+}
--- Dungeon Smiths/Assets/Minecart Game/Scripts/MinecartMove.cs
+++ Dungeon Smiths/Assets/Minecart Game/Scripts/MinecartMove.cs
@@ -6,6 +6,8 @@ public class MinecartMove : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D m_Rigidbody;
+    private bool inAir = false;
+    private float countdown = 1.0f;
 
     void Start()
     {
@@ -15,7 +17,40 @@ public class MinecartMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 vec = new Vector2(100.0f, -98.1f);
-        m_Rigidbody.AddRelativeForce(vec, ForceMode2D.Force);
+        transform.localEulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Mathf.Clamp(transform.rotation.eulerAngles.z, -40, 40));
+
+        if (inAir && countdown > 0.0f)
+        {
+            countdown -= Time.deltaTime;
+        }
+        else
+        {
+            countdown = 1.0f;
+            inAir = false;
+        }
+
+        if ((Input.GetMouseButton(0) || Input.touchCount > 0) && !inAir)
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+        //print(hit.normal);
+        //https://answers.unity.com/questions/27340/rotating-an-object-to-equal-normals-of-object-belo.html
+        if (hit.collider != null)
+        {
+            Vector2 vec = new Vector2(5.0f, -9.81f);
+            m_Rigidbody.AddRelativeForce(vec, ForceMode2D.Impulse);
+        }
+    }
+
+    void Jump()
+    {
+        Vector2 vec = new Vector2(0.0f, 100.0f);
+        m_Rigidbody.AddRelativeForce(vec, ForceMode2D.Impulse);
+        inAir = true;
     }
 }
