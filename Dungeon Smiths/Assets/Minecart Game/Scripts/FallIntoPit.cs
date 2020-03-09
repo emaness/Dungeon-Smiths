389c58b32b78b41a7863c27cce7e9b16f1353067
sashavol <sashavol@sashavol.com>
2020-03-08 18:20:59 -0700

    added minecart

---
+++ Dungeon Smiths/Assets/Minecart Game/Scripts/FallIntoPit.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallIntoPit : MonoBehaviour
+{
+    public GameObject gameObj;
+    private bool gameIsSolo = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int countLoaded = SceneManager.sceneCount;
+        Scene[] loadedScenes = new Scene[countLoaded];
+
+        for (int i = 0; i < countLoaded; i++)
+        {
+            loadedScenes[i] = SceneManager.GetSceneAt(i);
+        }
+
+        if (loadedScenes[0].name == "Minecart Game")
+        {
+            gameIsSolo = true;
+        }
+        print(gameIsSolo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!gameIsSolo)
+        {
+            SceneManager.UnloadSceneAsync("Minecart Game").completed += e =>
+            {
+                SceneManager.LoadScene("Minecart Game", LoadSceneMode.Additive);
+            };
+        }
+        else
+        {
+            SceneManager.LoadScene("Minecart Game");
+        }
+    }
+}
--- Dungeon Smiths/Assets/Minecart Game/Scripts/MinecartMove.cs
+++ Dungeon Smiths/Assets/Minecart Game/Scripts/MinecartMove.cs
@@ -6,6 +6,8 @@ public class MinecartMove : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D m_Rigidbody;
+    private bool inAir = false;
+    private float countdown = 1.0f;
 
     void Start()
     {
@@ -15,7 +17,44 @@ public class MinecartMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 vec = new Vector2(100.0f, -98.1f);
-        m_Rigidbody.AddRelativeForce(vec, ForceMode2D.Force);
+        //transform.localEulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Mathf.Clamp(transform.rotation.eulerAngles.z, -40, 40));
+
+        if (inAir && countdown > 0.0f)
+        {
+            countdown -= Time.deltaTime;
+        }
+        else
+        {
+            countdown = 1.0f;
+            inAir = false;
+        }
+
+        if ((Input.GetMouseButton(0) || Input.touchCount > 0) && !inAir)
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+        if (hit.collider != null)
+        {
+            float step = 130.0f * Time.deltaTime;
+            Vector2 target = new Vector2(988.3f, -4.6f);
+            m_Rigidbody.position = Vector2.MoveTowards(m_Rigidbody.position, target, step);
+            
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector3.up, hit.normal), 5 * Time.deltaTime);
+
+            //Vector2 vec = new Vector2(5.0f, -9.81f);
+            //m_Rigidbody.AddRelativeForce(vec, ForceMode2D.Impulse);
+        }
+    }
+
+    void Jump()
+    {
+        Vector2 vec = new Vector2(10.0f, 50.0f);
+        m_Rigidbody.AddRelativeForce(vec, ForceMode2D.Impulse);
+        inAir = true;
     }
 }
