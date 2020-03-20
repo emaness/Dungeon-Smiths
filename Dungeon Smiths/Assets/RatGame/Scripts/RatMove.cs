b81e4d78515d794ab8694d800f00ed988a258736
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2020-03-19 17:57:16 -0700

    initial push

---
+++ Dungeon Smiths/Assets/RatGame/Scripts/RatMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatMove : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec = new Vector3(5.0f, 0.0f, 5.0f);
+        m_Rigidbody.AddRelativeForce(vec);
+    }
+}
