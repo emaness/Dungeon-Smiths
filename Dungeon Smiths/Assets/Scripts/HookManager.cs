6b716ee72fbe7d1c63a0e3c38fe9b3ef50ae3d48
sashavol <sashavol@sashavol.com>
2020-04-01 03:14:20 -0700

    merged michael's and daniel's work

---
+++ Dungeon Smiths/Assets/Scripts/HookManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookManager : MonoBehaviour
+{
+    public RopeManager ropeManager;
+    private void Start()
+    {
+        ropeManager = GetComponentInParent<RopeManager>();
+        
+    }
+    private void Update()
+    {
+        if (ropeManager.rope_state == RopeState.elongation)   // when rope is enlongating, hook must be active
+        {
+            transform.GetComponent<PolygonCollider2D>().enabled = true;
+        }
+        if (ropeManager.length <= 1f)  // when hook go back to origin
+        {
+            if (transform.childCount > 0)   
+            {
+                Destroy(transform.GetChild(0).gameObject);
+            }
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "bomb")
+        { 
+            Debug.Log("game over");
+            return;
+        }
+        ropeManager.rope_state = RopeState.shorten;
+        collision.transform.parent = transform;  
+        collision.transform.localPosition = new Vector3(0,-1,0);        
+        transform.GetComponent<PolygonCollider2D>().enabled = false;
+        collision.GetComponent<PolygonCollider2D>().enabled = false;  // rock collider close
+        if (collision.transform.tag == "rock")
+        {
+            ropeManager.ScaleSpeed = 0.6f;
+        }
+     
+
+    }
+}
---
+++ Dungeon Smiths/Assets/Scripts/RopeManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RopeState
+{
+    idle,  //normal rotate
+    elongation, 
+    shorten
+}
+public class RopeManager : MonoBehaviour
+{
+
+    private Vector3 direction=new Vector3(0,0,1); //rotate direction (z axis)
+    public float length = 1f;   // length of rope
+    [HideInInspector]
+    public RopeState rope_state; // state of rope
+    public GameObject hook;
+    public float ScaleSpeed = 1;
+    public void Update()
+    {
+      
+        if (rope_state == RopeState.idle)
+        {
+            ScaleSpeed = 1;
+            RopeRotate();
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                rope_state = RopeState.elongation;
+            }
+        }
+        else if (rope_state == RopeState.elongation)
+        {
+            RopeElongation();
+        }
+        else if (rope_state == RopeState.shorten)
+        {
+            RopeShorten();
+        }
+        
+     
+
+    }
+    public void RopeRotate(){
+
+        if (transform.localRotation.z <= -0.5f)
+        {
+            direction = Vector3.forward;
+        }
+        else if (transform.localRotation.z >= 0.5f)
+        {
+            direction = Vector3.back;  
+        }
+        transform.Rotate(direction);
+    }
+    
+    public void RopeElongation()   // direction (y axis)
+    {
+      
+        if (length >= 6f)
+        {
+            rope_state = RopeState.shorten;
+            return;
+        }
+        length += Time.deltaTime*3*ScaleSpeed;
+        transform.localScale = new Vector3(transform.localScale.x, length, transform.localScale.z);
+        hook.transform.localScale= new Vector3(hook.transform.localScale.x, 1/length,hook.transform.localScale.z); //fix hook when rope elongates
+
+    }
+    public void RopeShorten()
+    {
+        if (length <= 1f)
+        {
+            rope_state = RopeState.idle;
+            return;
+        }
+        length -= Time.deltaTime*3*ScaleSpeed;
+        transform.localScale = new Vector3(transform.localScale.x, length, transform.localScale.z);
+        hook.transform.localScale = new Vector3(hook.transform.localScale.x, 1 / length, hook.transform.localScale.z);
+
+    }
+}
