0dcaa1ea4eaaa8f0db1767dfc76b11afa99cfc4c
Sasha Volokh <volokh@usc.edu>
2020-04-14 20:28:14 -0700

    integrated scorpion insect game

---
+++ Dungeon Smiths/Assets/ScorpionInsectGame/Lives.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Lives : MonoBehaviour{
+
+	private int lives =3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = "Lives: " + this.lives;
+    }
+
+    public IEnumerator DoLose()
+    {
+        GameObject.Find("WinText").SetActive(true);
+        GameObject.Find("BombSpawner").SetActive(false);
+        GameObject.Find("ScorpionSpawner").SetActive(false);
+        GameObject.Find("Sword").SetActive(false);
+
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.UnloadSceneAsync("ScorpionInsect").completed += e =>
+        {
+            SceneManager.LoadScene("ScorpionInsect", LoadSceneMode.Additive);
+        };
+
+        yield return null;
+    }
+
+    public void decreaseLives(int decreaseValue){
+    	this.lives -= decreaseValue;
+    	GetComponent<Text>().text = "Lives: " + this.lives;
+  		if (this.lives ==0){
+            StartCoroutine("DoLose");
+  		}
+    }
+    // Update is called once per frame
+    public int getLives(){
+    	return this.lives;
+    }
+}
---
+++ Dungeon Smiths/Assets/ScorpionInsectGame/Score.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Score : MonoBehaviour {
+ 
+	private int score = 0;
+	public int goal;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text>().text = "Score: " + this.score;
+	}
+
+    public IEnumerator DoWin()
+    {
+		GameObject.Find("WinText").SetActive(true);
+		GameObject.Find("BombSpawner").SetActive(false);
+		GameObject.Find("ScorpionSpawner").SetActive(false);
+		GameObject.Find("Sword").SetActive(false);
+
+		yield return new WaitForSeconds(2.0f);
+
+		SceneManager.UnloadSceneAsync("ScorpionInsect");
+		Scene mt = SceneManager.GetActiveScene();
+		foreach (GameObject obj in mt.GetRootGameObjects())
+        {
+			obj.SetActive(true);
+        }
+
+		yield return null;
+    }
+
+	public void incrementScore(int incrementValue){
+		this.score += incrementValue;
+		GetComponent<Text>().text = "Score: " + this.score;
+		if (score >= goal){
+			StartCoroutine("DoWin");
+		}
+	}
+
+	public int getScore() {
+		return this.score;
+	}
+}
